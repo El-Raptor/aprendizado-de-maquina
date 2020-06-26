818f5cd2e3d9049056ece78348e67dec96bc6d5a
holandah <54658970+holandah@users.noreply.github.com>
2020-06-25 21:03:17 -0400

    Add files via upload

--- trab2.py
+++ trab2.py
@@ -9,6 +9,21 @@
 
 import csv
 
+#caminho para os datasets
+dataset_path = [
+                "./datasets/arrhythmia/arrhythmia.data",
+                "./datasets/balance-scale/balance-scale.data",
+                "./datasets/blood-transfusion/transfusion.data",
+                "./datasets/echocardiogram/echocardiogram.data",
+                "./datasets/glass/glass.data",
+                "./datasets/haberman/haberman.data",
+                "./datasets/hepatitis/hepatitis.data",
+                "./datasets/iris/iris.data",
+                "./datasets/lymphography/lymphography.data",
+                "./datasets/tae/tae.data"
+                ]
+
+
 def read_input_file(file_path):
     open_file = open(file_path, "r")
     reader = csv.reader(open_file)
@@ -17,24 +32,53 @@ def read_input_file(file_path):
     return data
 
 
+
+def decision_tree(dataset):
+    return 1
+
+def knn(dataset):
+    return 2
+
+def naive_bayes(dataset):
+    return 3
+
+def regressao_logistica(dataset):
+    return 4
+
+def mlp_neural_networks(dataset):
+    return 5
+
+
+
 if __name__ == '__main__':
-    #print(read_input_file("./iris.data"))
 
-    #TODO: ler 10 datasets
-    
-    
-    #TODO: aplicar os cinco algoritmoscom no minimo 2 combinações
-    
-    
-    #TODO: medir desempenho
-    # 10-fold cross validation estratificado
-    # acuracia, f1_score
-    # media e desvio padrao
+    for i in range(10):
+        # As seguintes listas armazenam media e desvio padrao para acuracia e f1_score
+        metrics_dc = []
+        metrics_knn = []
+        metrics_nb = []
+        metrics_rg = []
+        metrics_mlp = []
 
-    #TODO: gerar informaçoes de saida
-    
-    
-    
-    
-    
-    
+        data = read_input_file(dataset_path[i])
+        print (data)
+
+        '''
+        combination = 2 # combination >= 2
+        #TODO: aplicar os cinco algoritmos com no minimo 2 combinações
+        for i in range(combination):
+            metrics_dc.append(decision_tree(data))
+
+            metrics_knn.append(knn(data))
+
+            metrics_nb.append(naive_bayes(data))
+
+            metrics_rg.append(regressao_logistica(data))
+
+            metrics_mlp.append(mlp_neural_networks(data))
+
+        #TODO: medir desempenho
+        # 10-fold cross validation estratificado
+        # acuracia, f1_score
+        # media e desvio padrao
+        '''
